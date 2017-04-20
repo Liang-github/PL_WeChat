f44a5f84894948c1122ae8114d969eef54f002e5
LIANG <pl941227@icloud.com>
2017-04-20 11:26:00 +0800

    发现

--- WeChat/DiscoverViewController.swift
+++ WeChat/DiscoverViewController.swift
@@ -8,28 +8,52 @@
 
 import UIKit
 
-class DiscoverViewController: UIViewController {
+class DiscoverViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    let titleArray = [["朋友圈"],["扫一扫","摇一摇"],["附近的人","漂流瓶"],["购物","游戏"],["小程序"]]
+    let imageArray = [["ff_IconShowAlbum"],["ff_IconQRCode","ff_IconShake"],["ff_IconLocationService","ff_IconBottle"],["CreditCard_ShoppingBag","MoreGame"],["FootStep"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let tableView = UITableView.init(frame: UIScreen.main.bounds, style: .grouped)
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 5
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else if section == 1 || section == 2 || section == 3 {
+            return 2
+        } else if section == 4 {
+            return 1
+        } else {
+            return 0
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: nil)
+        
+        let imageView = UIImageView.init(frame: CGRect(x: 10, y: 10, width: 24, height: 24))
+        imageView.image = UIImage(named: imageArray[indexPath.section][indexPath.row])
+        cell.contentView.addSubview(imageView)
+        
+        let label = UILabel.init(frame: CGRect(x: 44, y: 12, width: 100, height: 20))
+        label.text = titleArray[indexPath.section][indexPath.row]
+        cell.contentView.addSubview(label)
+        
+        return cell
     }
-    */
-
 }
